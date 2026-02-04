9a267abe3e3e706bee17cc68762c6b631f9e775c
Apn <apnkta@gmail.com>
2026-02-04 10:54:43 +0630

    chore: update server port to 3005

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = 3002;
+const PORT = 3005;
 
 // Middleware
 app.use(compression()); // Enable GZIP compression
